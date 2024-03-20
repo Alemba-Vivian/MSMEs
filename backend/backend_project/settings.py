a8b5794c9285cbd9548c447e0ce746634c9d5a25
fionamokua <mokuafiona@gmail.com>
2024-03-20 10:52:46 +0300

    cors

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     #3rd party apps
     "rest_framework",
+    'corsheaders',
+    
     
     #localapps
     "API",
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -126,3 +129,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CORS_ALLOWED_ORIGINS = [
+    # Add domains from which your frontend will make requests
+    "http://localhost:4200",
+  
+]
