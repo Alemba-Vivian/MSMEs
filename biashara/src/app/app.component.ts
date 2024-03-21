9a4d127d9e387e6d511c8a2111ecbf1e1c8e91b7
Alemba-Vivian <alembavivian@gmail.com>
2024-03-21 13:01:19 +0300

    form layout

--- biashara/src/app/app.component.ts
+++ biashara/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BiasharaFormComponent } from "./biashara-form/biashara-form.component";
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, BiasharaFormComponent]
 })
 export class AppComponent {
   title = 'biashara';
