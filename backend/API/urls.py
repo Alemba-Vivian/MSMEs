ebdd219dbc58116eb47c32b35d82b663a3344680
fionamokua <mokuafiona@gmail.com>
2024-03-26 09:43:58 +0300

    requirements

--- backend/API/urls.py
+++ backend/API/urls.py
@@ -3,6 +3,6 @@ from .views import ExternalStakeholdersList,ExternalStakeholderDetail
 
 
 urlpatterns = [
-    path("/",ExternalStakeholdersList.as_view(),name="list"),
-    path("/<int:pk>/",ExternalStakeholderDetail.as_view(),name="detail"),
+    path("",ExternalStakeholdersList.as_view(),name="list"),
+    path("<int:pk>/",ExternalStakeholderDetail.as_view(),name="detail"),
 ]
